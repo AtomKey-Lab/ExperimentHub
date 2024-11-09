cd2abb90ec561ba1f89bb602b81e3ac6441af33f
nehapansare <nehapansare007@gmail.com>
2024-11-09 13:55:26 +0530

    route added

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import Gravitation from './view/gravitation/gravitation'
 import reactDom from "react-dom/client"
 import Contact from './view/Contact/Contact'
 import Logbook from './view/logbook/logbook'
+import SignUp from './view/SignUp/SignUp'
+import Forgot from './view/Forgot/Forgot'
 import { BrowserRouter,Route,Routes } from "react-router-dom"
 
 const root =reactDom.createRoot(document.getElementById('root'))
@@ -20,6 +22,8 @@ root.render(
         <Route path="/Chemestry" element={<Chemestry/>}/>
         <Route path="/Contact" element={<Contact/>}/>
         <Route path="/logbook" element={<Logbook/>}/>
+        <Route path="/SignUp" element={<SignUp/>}/>
+        <Route path="/Forgot" element={<Forgot/>}/>
         
     </Routes>
     </BrowserRouter>
