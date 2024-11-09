ce73c6acd416094f2c30140dc5ed4e9eb39be679
nehapansare <nehapansare007@gmail.com>
2024-11-09 21:16:09 +0530

    footer enhance

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,22 +1,54 @@
-import React from "react";
-import footerConfig from "../../config/footer";
-import "./footer.css"; 
+import React from 'react';
+import './footer.css';
 
-const Footer = () => (
-  <footer className="footer">
-    <div className="container">
-      <ul className="footer-links">
-        {footerConfig.links.map((link, index) => (
-          <li key={index} className="footer-link-item">
-            <a href={link.href} className="footer-link">
-              {link.label}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <p className="footer-text">{footerConfig.text}</p>
-    </div>
-  </footer>
-);
+function Footer() {
+    return (
+        <footer className="footer">
+            <div className="footer-content">
+                <div className="footer-section">
+                    <h4>About Virtual Lab</h4>
+                    <p>Providing interactive simulations for Physics and Chemistry, making science more accessible and fun for students.</p>
+                </div>
+
+                <div className="footer-section">
+                    <h4>Need to Study</h4>
+                    <ul>
+                        <li>Chemistry Instruments</li>
+                        <li>Physics Instruments</li>
+                    </ul>
+                </div>
+
+                <div className="footer-section">
+                    <h4>Quick Links</h4>
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/simulations">Simulations</a></li>
+                        <li><a href="/logbook">Logbook</a></li>
+                        <li><a href="/contact">Contact</a></li>
+                    </ul>
+                </div>
+
+                <div className="footer-section">
+                    <h4>Contact Us</h4>
+                    <p>Email: info@virtuallab.com</p>
+                    <p>Phone: +1 (555) 123-4567</p>
+                </div>
+
+                <div className="footer-section">
+                    <h4>Follow Us</h4>
+                    <div className="social-icons">
+                        <a href="#"><img src="facebook-icon.png" alt="Facebook" /></a>
+                        <a href="#"><img src="twitter-icon.png" alt="Twitter" /></a>
+                        <a href="#"><img src="linkedin-icon.png" alt="LinkedIn" /></a>
+                    </div>
+                </div>
+            </div>
+
+            <div className="footer-bottom">
+                <p>&copy; 2024 Virtual Lab. All rights reserved.</p>
+            </div>
+        </footer>
+    );
+}
 
 export default Footer;
